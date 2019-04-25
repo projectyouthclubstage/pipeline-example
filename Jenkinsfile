8ad9caa4a6efe4b1418ca113c38c3a4891889625
sascha <sascha.deeg@gmail.com>
2019-04-25 20:48:51 +0200

    Init

---
+++ Jenkinsfile
@@ -0,0 +1,22 @@
+node{
+
+  // Pipeline Stages start here
+  // Requeres at least one stage
+
+
+    // Checkout source code
+    // This is required as Pipeline code is originally checkedout to
+    // Jenkins Master but this will also pull this same code to this slave
+    stage('Git Checkout') {
+      scm checkout
+    }
+
+    // Run Maven build, skipping tests
+    stage('Build'){
+        sh "mvn -B clean install -DskipTests=true"
+    }
+
+    // Run Maven unit tests
+    stage('Unit Test'){
+        sh "mvn -B test"
+    }
---
+++ src/main/java/com/example/demo/endpoint/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.example.demo.endpoint;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1")
+@RestController
+public class HelloWorld {
+
+
+    @RequestMapping("hello")
+    @GetMapping(produces = "application/json")
+    ResponseEntity<String> getHello(){
+        return new ResponseEntity<>("Hello",HttpStatus.OK);
+    }
+}
